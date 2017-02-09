9685eedacf3a9705da25e9ba700328bf8ee97b5d
Paweł Pietrasz <szpght@gmail.com>
2017-02-09 16:54:23 +0100

    enable ef

---
+++ MusicLibrary/DAL/LibraryContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace MusicLibrary.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("MusicLibraryConnString")
+        {
+        }
+    }
+}
