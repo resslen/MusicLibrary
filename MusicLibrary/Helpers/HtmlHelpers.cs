606f0c4fb1fb62ec8eb4b3cbf4df1161dda5a5a5
Paweł Pietrasz <szpght@gmail.com>
2017-02-10 21:14:07 +0100

    pretty table in indexes, common bar in edits and deletes

---
+++ MusicLibrary/Helpers/HtmlHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MusicLibrary.Helpers
+{
+    public static class HtmlHelpers
+    {
+        private static readonly object HtmlAttributes = new
+            {
+                @class = "waves-effect waves-light btn"
+            };
+
+        public static MvcHtmlString ActionButton(this HtmlHelper htmlHelper, string text, string action,
+            object routeValues = null)
+        {
+
+            return htmlHelper.ActionLink(text, action, routeValues, HtmlAttributes);
+        }
+
+        public static MvcHtmlString ActionButton(this HtmlHelper htmlHelper, string text, string action,
+            string controller, object routeValues)
+        {
+            return htmlHelper.ActionLink(text, action, controller, routeValues, HtmlAttributes);
+        }
+    }
+}
