63b7e891bd713fc51cfb006e6bdfcc5d60aa4ee7
Paweł Pietrasz <szpght@gmail.com>
2017-02-11 18:34:33 +0100

    showing tags in album edit

---
+++ MusicLibrary/Helpers/TagsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+using MusicLibrary.DAL;
+
+namespace MusicLibrary.Helpers
+{
+    public static class TagsHelpers
+    {
+        public static MvcHtmlString ListTags(this HtmlHelper helper, IEnumerable<Tag> tags)
+        {
+            var result = new StringBuilder();
+            foreach (var tag in tags)
+            {
+                result.Append(tag.Name + " ");
+            }
+            return new MvcHtmlString(result.ToString());
+        }
+    }
+}
